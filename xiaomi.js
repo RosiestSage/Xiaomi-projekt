78a210226e3a863a62544645b2e63c879bcd6622
Páli Dávid <pali.david@students.jedlik.eu>
2023-04-07 00:00:08 +0200

    indexjavascript

---
+++ xiaomi.js
@@ -0,0 +1,23 @@
+function gomb1() {
+    // var x = document.getElementById("szoveg")
+    // if (x.style.visibility === 'hidden'){
+    //     x.style.visibility = 'visible'
+    //     document.getElementById("szoveg").innerHTML = "                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ut delectus eveniet reiciendis sit voluptas temporibus accusantium! Nulla quam reprehenderit itaque, dolorum quisquam quaerat nam commodi dolorem ullam eligendi sed?"
+    // }
+    // else{
+    //     x.style.visibility = 'hidden'
+    // }
+    document.getElementById("szoveg").innerHTML = "                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ut delectus eveniet reiciendis sit voluptas temporibus accusantium! Nulla quam reprehenderit itaque, dolorum quisquam quaerat nam commodi dolorem ullam eligendi sed?"
+}
+
+function gomb2() {
+    document.getElementById("szoveg").innerHTML = "fasz"
+}
+
+function gomb3() {
+    document.getElementById("szoveg").innerHTML = "lofasz"
+}
+
+function gomb4() {
+    document.getElementById("szoveg").innerHTML = "nagyobb lofasz"
+}
