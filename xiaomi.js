d082f935f76be32aab6708a6163bed1e353577a9
Pali David <pali.david2007@gmail.com>
2023-04-08 12:20:22 +0200

    feligkészindex

--- xiaomi.js
+++ xiaomi.js
@@ -1,23 +1,15 @@
 function gomb1() {
-    // var x = document.getElementById("szoveg")
-    // if (x.style.visibility === 'hidden'){
-    //     x.style.visibility = 'visible'
-    //     document.getElementById("szoveg").innerHTML = "                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ut delectus eveniet reiciendis sit voluptas temporibus accusantium! Nulla quam reprehenderit itaque, dolorum quisquam quaerat nam commodi dolorem ullam eligendi sed?"
-    // }
-    // else{
-    //     x.style.visibility = 'hidden'
-    // }
-    document.getElementById("szoveg").innerHTML = "                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ut delectus eveniet reiciendis sit voluptas temporibus accusantium! Nulla quam reprehenderit itaque, dolorum quisquam quaerat nam commodi dolorem ullam eligendi sed?"
+    document.getElementById("szoveg").innerHTML = "A Xiaomit 2010. április 6-án tíz partner alapította meg. 2010. augusztus 16-án a Xiaomi hivatalosan is kiadta saját fejlesztésű, Android alapú operációs rendszerének, a MIUI-nak első verzióját." 
 }
 
 function gomb2() {
-    document.getElementById("szoveg").innerHTML = "fasz"
+    document.getElementById("szoveg").innerHTML = "2012 augusztusában a vállalat piacra dobta az Mi2 okostelefont, amelybe a Qualcomm Snapdragon S4 Pro APQ8064 egylapkás rendszerét (SoC), a Krait 1,5 GHz-es négymagos chipjét, 2 GB RAM-ot és az Adreno 320 GPU-t építették be."
 }
 
 function gomb3() {
-    document.getElementById("szoveg").innerHTML = "lofasz"
+    document.getElementById("szoveg").innerHTML = "2014-ben a Xiaomi bejelentette, hogy Kínán kívül is terjeszkedni kezd, elsőként Szingapúrban. Ez év február 21-én a vállalat piacra dobta Szingapúrban a Redmi okostelefont. 2014. március 17-én bejelentették a RedMi Note eszközt"
 }
 
 function gomb4() {
-    document.getElementById("szoveg").innerHTML = "nagyobb lofasz"
+    document.getElementById("szoveg").innerHTML = "2015 áprilisában a Xiaomi bejelentette, hogy két indiai e-kereskedelmi oldalon keresztül is elérhetővé teszi Mi készülékeit. 2015. április 23-án bejelentették, hogy bevezetik az indiai piacra a cég legújabb zászlóshajóját, az Mi 4i eszközt. 2015. június 30-án a Xiaomi a brazil piacra is belépett a helyben gyártott Redmi 2 eszközzel. "
 }
